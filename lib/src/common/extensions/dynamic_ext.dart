f640afb434dedc9b0d8b8006247d9fc6d569adbc
thanh.vo@viez.vn <thanh.vo@viez.vn>
2023-05-30 14:53:45 +0700

    Added extensions

--- lib/src/common/extensions/dynamic_ext.dart
+++ lib/src/common/extensions/dynamic_ext.dart
@@ -1,5 +1,9 @@
+import 'package:a01r066/src/common/get_utils/get_utils.dart';
+
 extension DynamicX on dynamic {
   String stringify() {
     return '{$runtimeType: ${jsonEncode(this)}}';
   }
+
+  bool? get isBlank => GetUtils.isBlank(this);
 }
